3a2899c60c7d77709d2437897d22b47a2a69997d
user <user@JOSEPH>
2018-02-14 11:58:41 +0900

    4th Trying

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -2,37 +2,32 @@ import java.util.Scanner;
 
 public class BaseballGame {
 	
-	public static int[] comNumber;
-	public static int[] userNumber;
-	
-	public static int[] ranNumList() {
+	public static int[] ranNumList(int[] number) {
 		int tmp,random;
-		comNumber = new int[] {1,2,3,4,5,6,7,8,9};
-		for(int i=0; i<comNumber.length; i++) {
-			random = (int)(Math.random()*comNumber.length);
-			random = Math.min(random,comNumber.length-1); // 인덱스 숫자 필요
+		for(int i=0; i<number.length; i++) {
+			random = (int)(Math.random()*number.length);
+			random = Math.min(random,number.length-1); // 인덱스 숫자 필요
 			// swap
-			tmp = comNumber[i];
-			comNumber[i] = comNumber[random];
-			comNumber[random] = tmp;
+			tmp = number[i];
+			number[i] = number[random];
+			number[random] = tmp;
 		}
-		return comNumber;
+		return number;
 	}
 	
 	
-	public static int[] userInput() {
+	public static int[] userInput(int[] number) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("숫자를 입력해 주세요 ex)123 : ");
 		int userNum = scan.nextInt();
-		userNumber = new int[3];
 		for(int i = 2; i >= 0; i--) {
-			userNumber[i] = userNum%10;
+			number[i] = userNum%10;
 			userNum = userNum/10;
 		}
-		return userNumber;
+		return number;
 	}
 	
-	public static int countStrike() {
+	public static int countStrike(int[] comNumber, int[] userNumber) {
 		int strike = 0;
 		for(int i=0; i<3; i++) {
 			if (comNumber[i]==userNumber[i]) strike++;
@@ -40,43 +35,53 @@ public class BaseballGame {
 		return strike;
 	}
 	
-	public static int countBall() {
+	public static int countBall(int[] comNumber, int[] userNumber) {
 		int ball = 0;
-		if(comNumber[0]==userNumber[1] || comNumber[0] == userNumber[2]) ball++;
-		if(comNumber[1]==userNumber[0] || comNumber[1] == userNumber[2]) ball++;
-		if(comNumber[2]==userNumber[0] || comNumber[2] == userNumber[1]) ball++;
+		for(int i=0;i<3;i++) {
+            countBall2(i,comNumber,userNumber);
+        }
 		return ball;
-	}3
+	}
 	
-	public static void display() {
+	private static int countBall2(int b, int[] comNumber, int[] userNumber) {
+		int ball = 0;
+		for(int j=0;j<3;j++) {
+            if (b!=j && comNumber[b] == userNumber[j]) ball++;
+        }
+		return ball;
+	}
+	
+	public static void display(int strike, int ball) {
 		
-		if (countStrike() > 0 || countBall() > 0) { 
-            System.out.println(countStrike() + " 스트라이크 " + countBall() + " 볼");
+		if (strike > 0 || ball > 0) { 
+            System.out.println(strike + " 스트라이크 " + ball + " 볼");
 		}
 		
-		if (countStrike() == 0 && countBall() == 0) {
+		if (strike == 0 && ball == 0) {
 			System.out.println("낫싱");
 		}
 	}
 	
 	public static void playGame() {
-		ranNumList();
+		int[] comNumber = new int[]{1,2,3,4,5,6,7,8,9};
+		int[] userNumber = new int[3];
+		ranNumList(comNumber);
 		while(true) {
-			userInput();
-			countStrike();
-			if(countStrike()==3) {
+			int s,b;
+			userInput(userNumber);
+			s = countStrike(comNumber,userNumber);
+			if(s==3) {
 				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 				break;
 			}
-			countBall();
-			display();
+			b = countBall(comNumber, userNumber);
+			display(s,b);
 			
 		}
 	}
 
 	public static void main(String[] args) {
-		playGame();
-	
+		playGame();	
 	}
 
 }
